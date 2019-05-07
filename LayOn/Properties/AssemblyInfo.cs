55720d121ef841c08edbc40184a0b63883d2f99e
Lino Cesar Cutini <lino.cc@brametal.com.br>
2019-05-07 10:49:53 -0300

    Recompilado

--- LayOn/Properties/AssemblyInfo.cs
+++ LayOn/Properties/AssemblyInfo.cs
@@ -9,11 +9,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ChangeTextInVariousDrawings")]
+[assembly: AssemblyTitle("LayOn")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ChangeTextInVariousDrawings")]
+[assembly: AssemblyProduct("LayOn")]
 [assembly: AssemblyCopyright("Copyright 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
