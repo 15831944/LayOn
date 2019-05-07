5bb54e275138b9192bcc817e4b39cb6c5075e6f3
Lino Cesar Cutini <lino.cc@brametal.com.br>
2019-05-07 10:46:05 -0300

    Primeiro Commit

---
+++ LayOn/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Autodesk.AutoCAD.Interop;
+using Autodesk.AutoCAD.Interop.Common;
+
+namespace ChangeTextInVariousDrawings
+{
+    class Program
+    {
+        private static string[] arquivos;
+
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            if (args.Length != 0)
+            {
+                arquivos = args;
+            }
+            else
+            {
+                SelectFiles();
+            }
+
+            AcadApplication acApp = null;
+
+            try
+            {
+                acApp = Marshal.GetActiveObject("AutoCAD.Application") as AcadApplication;
+            }
+            catch
+            {
+                MessageBox.Show("Não foi possível abrir o AutoCAD");
+            }
+
+            foreach (var desenho in arquivos)
+            {
+                AcadDocument doc;
+
+                try
+                {
+                    doc = acApp.Documents.Open(desenho, false, string.Empty);
+
+                    AcadModelSpace modelSpace = doc.ModelSpace;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Não foi possível abrir o desenho " + desenho + ex);
+                    break;
+                }
+
+                doc.SendCommand("LAYON ");
+
+                doc.PurgeAll();
+                doc.PurgeAll();
+                doc.PurgeAll();
+                acApp.ZoomExtents();
+                doc.Save();
+                doc.Close();
+            }
+        }
+
+        static void SelectFiles()
+        {
+
+            // Displays an OpenFileDialog so the user can select a Cursor.
+            OpenFileDialog openFileDialog2 = new OpenFileDialog();
+            openFileDialog2.Filter = "Drawing Files|*.dwg";
+            openFileDialog2.Title = "Selecione os arquivos DWG";
+            openFileDialog2.RestoreDirectory = true;
+            openFileDialog2.Multiselect = true;
+
+            if (openFileDialog2.ShowDialog() == DialogResult.OK)
+            {
+                // Guarda a lista dos caminhos completos dos arquivos selecionados
+                arquivos = openFileDialog2.FileNames;
+            }
+            else
+            {
+                Console.WriteLine("Arquivos não selecionados!");
+                Console.ReadKey();
+            }
+        }
+    }
+}
